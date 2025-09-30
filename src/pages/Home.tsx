ce953f29648a6ea7f7690159f6b10bd265f34c75
BASIS-64 Computer <basis64computer@gmail.com>
2025-09-30 17:01:10 +0800

    UX Update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -24,7 +24,7 @@ export default function Home() {
 					src={bgImage}
 					alt="Header Background"
 					className="absolute inset-0 w-full h-full object-cover opacity-60"
-					loading="eager"
+					loading="lazy"
 					// @ts-ignore
 					fetchpriority="high"
 				/>
